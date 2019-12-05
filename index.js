cc5f7271812598d4b6197ab13988db76fffebbd9
jim.schumacher <jim.schumacher@workday.com>
2019-12-05 11:05:31 -0800

    Adds day for and other fixes

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 const _ = require('lodash');
 const aocUtils = require('./aocUtils');
 const day5 = require('./day5intcode');
+const day4 = require('./day4passwd');
 
-module.exports = _.assign(aocUtils, day5); 
+module.exports = _.assign(aocUtils, day4, day5);
+
+const allTests = _.pick(module.exports, _.keys(module.exports).filter(k => k.endsWith('runTests')));
+
+module.exports.runAllTests = () => {
+  _.keys(allTests).forEach(t => allTests[t]());
+}
