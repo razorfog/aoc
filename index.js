67838192534f97b8dbb336172dc11cde54df7cef
jim.schumacher <jim.schumacher@workday.com>
2019-12-05 20:06:32 -0800

    Add day2

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const aocUtils = require('./aocUtils');
 const day5 = require('./day5intcode');
 const day4 = require('./day4passwd');
 const day3 = require('./day3wires');
+const day2 = require('./day2intcode');
 
-module.exports = _.assign(aocUtils, day3, day4, day5);
+module.exports = _.assign(aocUtils, day2, day3, day4, day5);
 
 const allTests = _.pick(module.exports, _.keys(module.exports).filter(k => k.endsWith('runTests')));
 
