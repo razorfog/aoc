85dffa79f610066d6ecb0c228bb5b03c0c1606ff
James Schumacher <james@unitive.works>
2020-12-06 15:40:40 -0800

    Refactors to day5

--- day5.py
+++ day5.py
@@ -1,29 +1,27 @@
+from aocday import AocDay
 
-import sys
+class Day5(AocDay):
+    def __init__(self, test_file=None):
+        super().__init__(5, test_file)
+        self.converter = str.maketrans(dict(F='0', L='0', B='1', R='1'))
 
-converter = str.maketrans(dict(F='0', L='0', B='1', R='1'))
+    def read_input(self, passfile):
+        with open(passfile) as f:
+            return set(map(lambda bp: int(bp.translate(self.converter), 2), f))
 
-def read_seat_ids(passfile: str):
-    with open(passfile) as f:
-        return set(map(lambda bp: int(bp.translate(converter), 2), f))
+    def part1(self, seat_ids):
+        print(f'Highest seat ID: {max(seat_ids)}')
 
-def part1(seat_ids):
-    return max(seat_ids)
-
-def part2(seat_ids):
-    # make a set that's from the range of the min seat to the max seat.
-    # the difference will be our seat_id
-    all_seats = set(range(min(seat_ids), max(seat_ids)))
-    my_seat_ids = all_seats.difference(seat_ids)
-    #  print(my_seat_ids)
-    return my_seat_ids.pop()
+    def part2(self, seat_ids):
+        # make a set that's from the range of the min seat to the max seat.
+        # the difference will be our seat_id
+        all_seats = set(range(min(seat_ids), max(seat_ids)))
+        my_seat_ids = all_seats.difference(seat_ids)
+        #  print(my_seat_ids)
+        print(f'Your seat id is: {my_seat_ids.pop()}')
 
 if __name__ == '__main__':
-    files = sys.argv[1::] if len(sys.argv) > 1 else ['./day5_input.txt']
-    for f in files:
-        print(f'=== {f} ===')
-        ids = read_seat_ids(f)
-        print(f'Highest seat ID: {part1(ids)}')
-        print(f'Your seat id is: {part2(ids)}')
+    Day5().run()
+
 
 
