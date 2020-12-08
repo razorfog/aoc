4718cc2f584fbe5955a2f91068730d5564aa93e3
Jim Schumacher <jschumacher@splunk.com>
2020-12-07 22:28:12 -0800

    day 8 clean up

--- day8.py
+++ day8.py
@@ -7,13 +7,13 @@ class Day8(AocDay):
 
     def __init__(self, test_file=None):
         super().__init__(8, test_file)
-        self.op_re = re.compile(r'(nop|acc|jmp)\s+([-+]\d+)')
 
     def read_input(self, code_file):
+        op_re = re.compile(r'(nop|acc|jmp)\s+([-+]\d+)')
         with open(code_file, 'r') as f:
-            return [[op, int(arg)] for (op, arg) in re.findall(self.op_re, f.read())]
+            return [[op, int(arg)] for (op, arg) in re.findall(op_re, f.read())]
 
-    def part1(self, rules):
+    def part1(self, rules, quiet=False):
         accval = 0
         pos = 0
         been_here = set()
@@ -33,17 +33,13 @@ class Day8(AocDay):
                 accval += arg
             else:
                 pos += arg
-        print(f'accumulator: {accval}')
+        if not quiet:
+            print(f'part-1: accumulator: {accval}')
         return (loop_detected, pos, accval)
 
 
     def part2(self, rules):
-        """
-        The program is supposed to terminate by attempting to execute an instruction immediately
-        after the last instruction in the file.
-        by changine one jmp or nop a loop will not accur
-        """
-        # try brute force:
+        # brute force:
         pos = 0
         while pos < len(rules):
             instr = rules[pos]
@@ -55,18 +51,12 @@ class Day8(AocDay):
                 instr[0] = "jmp"
             else:
                 instr[0] = "nop"
-            tx = rules[pos]
-            (loop_detected, last_pos, accval) = self.part1(rules)
+            (loop_detected, last_pos, accval) = self.part1(rules, quiet=True)
             instr[0] = ins_save
             if not loop_detected and last_pos == len(rules):
-                print(f'Found it: acc is {accval}')
+                print(f'Found it, part 2: acc is {accval}')
                 break
             pos += 1
-
-
-
-        return 0
-
 
 if __name__ == '__main__':
     Day8().run()
