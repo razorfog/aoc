240616084a5a638acbd6398291254d43504d199a
Jim Schumacher <transeme@yahoo.com>
2022-12-04 20:46:08 -0800

    Use bash to launch awk

--- 2021/day2.sh
+++ 2021/day2.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/env awk -f
+#!/usr/bin/env bash
+input="${1:-day2.input}"
+echo $input
+awk '
    /forward/ { horiz += $2;
                depth2 += $2 * aim; }
    /down/    { depth1 += $2; aim += $2; }
@@ -6,7 +9,9 @@
    END       { 
        printf("day2 pt 1: %d\n", depth1*horiz);
        printf("     pt 2: %d\n", depth2*horiz);
-   }
+   }' "$input"
+
+
 
 
 
