797de69eae766cdb4a3e0176cc276cde1f658ba9
Jim Schumacher <transeme@yahoo.com>
2022-12-06 11:38:45 -0800

    Use set, tho slower, more compact code

--- 2022/day6.js
+++ 2022/day6.js
@@ -4,18 +4,13 @@ function firstUnique(line, s=4) {
     let c = {};
     for (let i = 0; i < line.length - s; i += 1) {
         const m = {};
-        let unique = true;
-        for (let j = i; j < i+s && unique; j++ ) {
-            const c = line[j];
-            if (!m[c]) m[c]=true;
-            else
-                unique=false;
-        }
-        if (unique) {
-            console.log("Seg size %d: Unique at position %d - %d: %s", s, i, i+s,line.substring(i, i + s));
-            break;
+        const section = new Set(line.slice(i, i+s));
+        if (section.size == s) {
+            console.log("Sig size: %d Unique at index %d: %s", s, i+s, line.slice(i, i+s));
+            return;
         }
     }
+    console.log("No luck. Didint' find it.");
 }
 function run(input_file = 'day6.txt') {
     const lines = fs.readFileSync(input_file, 'utf8').split('\n');
@@ -23,6 +18,7 @@ function run(input_file = 'day6.txt') {
     firstUnique(lines[0], 14);
 }
 
+run();
 module.exports = {
     test: () => run('day6_test.txt'),
     run,
