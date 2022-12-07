141ae1275ce1426f25ecbcdbfe32782820117af6
Jim Schumacher <transeme@yahoo.com>
2022-12-07 00:44:53 -0800

    Day 7, remove debugging

--- 2022/day7.js
+++ 2022/day7.js
@@ -1,145 +1,105 @@
 const fs = require('fs');
 
 class Dir {
-    constructor(name, parent) {
-        this.name = name;
-        this.size = 0;
-        this.files = [];
-        this.dirs = [];
-        this.parent = parent;
-    }
+  constructor(name, parent) {
+    this.name = name;
+    this.size = 0;
+    this.files = [];
+    this.dirs = [];
+    this.parent = parent;
+  }
 
-    getFullName() {
-        return (this.parent == null) ? this.name : this.parent.getFullName() + "/" + this.name;
-    }
-    getDir(name) {
-        return this.dirs.find(d => d.name == name);
-    }
+  getDir(name) {
+    return this.dirs.find(d => d.name == name);
+  }
 
-    getFile(name) {
-        return this.files.find(f => f.name == name);
-    }
-    addFile(name, size) {
-        const exist = this.getFile(name);
-        if (exist) {
-            throw new Exception("duplicate file");
-        }
-        this.size += size;
-        this.files.push({name, size});
-    }
-    addDir(name) {
-        const exist = this.getDir(name);
-        if (exist) {
-            throw new Exception(`Duplicate dir ${name}`);
-        }
-        this.dirs.push(new Dir(name, this));
-    }
+  addFile(name, size) {
+    this.size += size;
+    this.files.push({name, size});
+  }
 
-    getSize() { return this.size; }
-    getTotalSize() {
-        let tsize = 0;
-        for (const dd of this.dirs) {
-            const xs = dd.getTotalSize();
-            tsize += xs;
-        }
-        return this.size + tsize;
-    }
-}
+  addDir(name) {
+    this.dirs.push(new Dir(name, this));
+  }
 
-function getDirsWithSize(xdir, size, result) {
-    const allSizes = new Map();
-    function gdws(d) {
-        const tsize = d.getTotalSize();
-        allSizes.set(d, tsize);
-        if (tsize <= size)
-           result.set(d, tsize);
-        else {
-            console.log("+++ %s overlimit: %d", d.name, tsize);
-        }
-        for (const xd of d.dirs) {
-            gdws(xd);
-        }
-    }
-    gdws(xdir);
-    console.log("---- ok DIRS ----");
+  getTotalSize() {
     let tsize = 0;
-    for (const [k,v] of result.entries()) {
-        console.log("dir %s => %d", k.name, v);
-        tsize += v;
+    for (const dd of this.dirs) {
+      const xs = dd.getTotalSize();
+      tsize += xs;
     }
-    console.log("Part1: ", tsize);
-    return allSizes;
+    return this.size + tsize;
+  }
 }
 
-function traverse(xdir, level = "- ") {
-    console.log("%s%s (dir) size = %d", level, xdir.name, xdir.getTotalSize());
-    const nextLevel = "  " + level;
-    for (const d of xdir.dirs) {
-        traverse(d, " " + nextLevel);
+function getDirsWithSize(xdir, size) {
+  const allSizes = new Map();
+  let result = 0; // part 1.
+  function gdws(d) {
+    const tsize = d.getTotalSize();
+    allSizes.set(d, tsize);
+    if (tsize <= size)
+      result += tsize;
+    for (const xd of d.dirs) {
+      gdws(xd);
     }
-    for (const f of xdir.files) {
-        console.log("%s%s (file, size=%d)",nextLevel, f.name, f.size);
-    }
+  }
+
+  gdws(xdir);
+  console.log("Part1: ", result);
+  return allSizes;
 }
+
 function run(input_file = 'day7.txt') {
-    const lines = fs.readFileSync(input_file, 'utf8').split('\n').filter(Boolean);
-    let cwd = null;
-    let topDir = null;
-    for (let i = 0; i < lines.length; i++) {
-        const x = lines[i];
-        if (x.startsWith('$')) {
-            const [cmd, arg] = x.substring(2).split(' ');
-            if (cmd == "cd") {
-                if (arg == "/") {
-                    if (topDir == null)
-                        topDir = new Dir(arg, null);
-                    cwd = topDir;
-                } else if (arg == "..") {
-                    cwd = cwd.parent;
-                } else {
-                    cwd = cwd.getDir(arg);
-                    if (!cwd) {
-                        console.error("No such dir %s", arg);
-                    }
-                }
-            }
-            else if (cmd == "ls") {
-                let j;
-                for (j = i+1; j < lines.length; j++) {
-                    const y = lines[j];
-                    if (y.startsWith('$')) {
-                        break;
-                    }
-                    const [kind, name ] = y.split(' ');
-                    if (kind != "dir") {
-                        cwd.addFile(name, Number(kind));
-                    } else cwd.addDir(name);
-                }
-                i = j-1;
-            }
+  const lines = fs.readFileSync(input_file, 'utf8').split('\n').filter(Boolean);
+  let cwd = null;
+  let topDir = null;
+  for (let i = 0; i < lines.length; i++) {
+    const x = lines[i];
+    if (x.startsWith('$')) {
+      const [, cmd, arg] = x.split(' ');
+      if (cmd == "cd") {
+        if (arg == "/") {
+          if (topDir == null)
+            topDir = new Dir(arg, null);
+          cwd = topDir;
+        } else if (arg == "..") {
+          cwd = cwd.parent;
+        } else {
+          cwd = cwd.getDir(arg);
+          if (!cwd) {
+            console.error("No such dir %s", arg);
+          }
         }
-    }
-    //traverse(topDir);
-    const result = new Map();
-    const allSizes = getDirsWithSize(topDir, 100000, result);
-    const totalSpace = 70000000;
-    const needFreeSpace = 30000000;
-    const unusedSpace = totalSpace - topDir.getTotalSize();
-    const needSpace = needFreeSpace - unusedSpace;
-    console.log("Need %d", needSpace);
-    let closest = totalSpace;
-    let bestDir;
-    for (const [d, size] of allSizes.entries() ) {
-        if (size > needSpace && size < closest) {
-            closest = size;
-            bestDir = d;
+      } else if (cmd == "ls") {
+        let j;
+        for (j = i + 1; j < lines.length; j++) {
+          const y = lines[j];
+          if (y.startsWith('$')) {
+            break;
+          }
+          const [kind, name] = y.split(' ');
+          if (kind != "dir") {
+            cwd.addFile(name, Number(kind));
+          } else cwd.addDir(name);
         }
+        i = j - 1;
+      }
     }
-    console.log("best path: %s. Size = %d", bestDir.getFullName(), closest);
+  }
+  //traverse(topDir);
+  const allSizes = getDirsWithSize(topDir, 100000);
+  // part 2.
+  const totalSpace = 70000000;
+  const needFreeSpace = 30000000;
+  const unusedSpace = totalSpace - topDir.getTotalSize();
+  const needSpace = needFreeSpace - unusedSpace;
+  const best = Math.min(...[...allSizes.values()].filter(sz => sz >= needSpace));
+  console.log("Part 2. best = %d", best);
 }
 
-// run('day7.txt');
+ run('day7.txt');
 module.exports = {
-    test: () => run('day7_test.txt'),
-    run,
+  test: () => run('day7_test.txt'),
+  run,
 };
